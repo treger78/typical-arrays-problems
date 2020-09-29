6a17bdb15ab7a53d0ad6a4874ddeec436fd05715
Ilya <51325702+treger78@users.noreply.github.com>
2020-09-29 13:18:01 +0300

    feat: make min, max & avg func

--- src/index.js
+++ src/index.js
@@ -1,12 +1,38 @@
 
 exports.min = function min (array) {
-  return 0;
+	let mi = Number.MAX_SAFE_INTEGER;
+	if (arguments.length === 0 || array.length === 0) {
+		return 0;
+	}
+	for (var i = 0; i < array.length; i++) {
+		if (array[i] < mi) {
+			mi = array[i];
+		}
+	}
+  return mi;
 }
 
 exports.max = function max (array) {
-  return 0;
+	let ma = 0;
+	if (arguments.length === 0 || array.length === 0) {
+		return 0;
+	}
+	for (var i = 0; i < array.length; i++) {
+		if (array[i] > ma) {
+			ma = array[i];
+		}
+	}
+  return ma;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+	let av = 0;
+	if (arguments.length === 0 || array.length === 0) {
+		return 0;
+	}
+	for (var i = 0; i < array.length; i++) {
+		av += array[i];
+	}
+	av /= array.length;
+  return av;
 }
